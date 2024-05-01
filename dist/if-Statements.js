c5c2de5385093c32286ebdcfb36327e1ce6ac4b0
Hassam Arif <arifhassam18@gmail.com>
2024-05-02 00:25:51 +0500

    if statements

---
+++ dist/if-Statements.js
@@ -0,0 +1,10 @@
+"use strict";
+let num1 = 10;
+let num2 = 33;
+let num3 = 15;
+num1 > num2
+    ? console.log(`${num1} is greater than ${num2}`)
+    : console.log(`${num2} is greater than ${num1}`);
+num1 > num3
+    ? console.log(`${num1} is greater than ${num3}`)
+    : console.log(`${num3} is greater than ${num1}`);
---
+++ src/if-Statements.ts
@@ -0,0 +1,20 @@
+let num1: number = 10;
+// let num2: number = 3;
+let num2: number = 33;
+let num3: number = 15;
+
+// if (num1 > num2) console.log(`${num1} is greater than ${num2}`);
+
+// if (num1 > num3) {
+//   console.log(`${num1} is greater than ${num3}`);
+// } else {
+//   console.log(`${num3} is greater than ${num1}`);
+// }
+
+num1 > num2
+  ? console.log(`${num1} is greater than ${num2}`)
+  : console.log(`${num2} is greater than ${num1}`);
+
+num1 > num3
+  ? console.log(`${num1} is greater than ${num3}`)
+  : console.log(`${num3} is greater than ${num1}`);
