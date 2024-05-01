f783cde942481f932045cbc74d990c3b8e0764b4
Hassam Arif <arifhassam18@gmail.com>
2024-05-01 23:10:55 +0500

    tuples

---
+++ dist/tuple.js
@@ -0,0 +1,4 @@
+"use strict";
+const courseDetails = ["CSS, Layouts, MasterClass", 80.9];
+const courseDetails1 = [80.9, "CSS, Layouts, MasterClass"];
+console.log(courseDetails);
---
+++ src/tuple.ts
@@ -0,0 +1,3 @@
+const courseDetails: [string, number] = ["CSS, Layouts, MasterClass", 80.9];
+const courseDetails1: [number, string] = [80.9, "CSS, Layouts, MasterClass"];
+console.log(courseDetails);
