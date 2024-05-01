df611288bac802a747b655f1cbfdac6cc7b1bc49
Hassam Arif <arifhassam18@gmail.com>
2024-05-01 23:11:21 +0500

    tuples

--- src/tuple.ts
+++ src/tuple.ts
@@ -1,3 +1,4 @@
 const courseDetails: [string, number] = ["CSS, Layouts, MasterClass", 80.9];
+//number and string must be in same order as written...
 const courseDetails1: [number, string] = [80.9, "CSS, Layouts, MasterClass"];
 console.log(courseDetails);
